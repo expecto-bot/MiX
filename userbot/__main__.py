0a6e163a7e8887cc7b6b67a29adca2c786f242a8
jkthe1 <70848090+jkthe1@users.noreply.github.com>
2020-09-29 01:07:41 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -29,7 +29,7 @@ for module_name in ALL_MODULES:
 LOGS.info(f"You are running MiX [{BOT_VER}]")
 
 LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .start/.ping in any chat."
+    "Congratulations, your userbot is now running !! Test it by typing .alive /.ping in any chat."
     "\nIf you need assistance, head to https://t.me/PPE_Support")
     
     
